50a58363b921f95a941630b8dcbc32560de6f2d7
AH-Aashiq <arifulhaq215@gmail.com>
2022-11-29 03:35:31 +0600

    solve added

---
+++ 200-1200/lunchTime.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	int x;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x;
+	    if(x>=1&&x<=4)
+	    cout<<"YES"<<endl;
+	    else 
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
