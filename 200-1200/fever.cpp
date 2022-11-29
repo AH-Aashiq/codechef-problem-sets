f1983a37af04c8ecdf3f23a9c299134276d70e23
AH-Aashiq <arifulhaq215@gmail.com>
2022-11-30 01:23:20 +0600

    solve added

---
+++ 200-1200/fever.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int t = 0;
+    cin >> t;
+    
+    while(t--)
+    {
+        int x = 0;
+        cin >> x;
+        
+        if(x > 98)
+        cout << "YES\n";
+        
+        else
+        cout << "NO\n";
+       
+    }
+	return 0;
+}
